da551b828b6c51865f1af97aa0a64ac310100749
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2023-12-05 23:50:53 +1100

    Add functions with more arguments
    Up to 6 where cosmo fails to pass all parameters.
    I have a fix for this specific case but it cannot handle stack parameter passing so any won't work, at least on x86_64 sysv systems.

--- src/addtest.c
+++ src/addtest.c
@@ -1,6 +1,17 @@
+#include <assert.h>
 #include <stdio.h>
 #include <testlib.h>
 
+#define test(type, specifier, actual, expected) { \
+  type act = actual;\
+  type exp = expected;\
+  printf(#actual " = %d\n", act);\
+  assert(act == exp);\
+}
 int main(void) {
-  printf("add2(1, 2) = %d\n", add2(1, 2));
+  test(int, "d", add2(1, 2), 3);
+  test(int, "d", add3(1, 2, 3), 6);
+  test(int, "d", add4(1, 2, 3, 4), 10);
+  test(int, "d", add5(1, 2, 3, 4, 5), 15);
+  test(int, "d", add6(1, 2, 3, 4, 5, 6), 21);
 }
--- src/testlib.h
+++ src/testlib.h
@@ -2,5 +2,9 @@
 #define TESTLIB_H_
 
 int add2(int, int);
+int add3(int, int, int);
+int add4(int, int, int, int);
+int add5(int, int, int, int, int);
+int add6(int, int, int, int, int, int);
 
 #endif /* TESTLIB_H_ */
