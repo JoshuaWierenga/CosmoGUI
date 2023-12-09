f3dfbc5850708b72b2537bbebfd99470d33ba0fc
Joshua Wierenga <joshuagit@joshuawierenga.tech>
2023-12-09 17:12:11 +1100

    Add more examples from raygui and raylib-games
    Anything using raygui will need to use raygui_fix.c until I can get varargs functions working.

--- build.sh
+++ build.sh
@@ -17,7 +17,7 @@ GENERATED=$PWD/src/generated/
 CORES=$(($(nproc) + 1))
 
 rm -rf "${OUTPUT:?}/" "${GENERATED:?}/"
-mkdir -p "$OUTPUT/include/" "$OUTPUT/lib/" "$GENERATED/libraylib/" "$GENERATED/libraylib_wrapper/"
+mkdir -p "$OUTPUT/bin/resources/" "$OUTPUT/include/" "$OUTPUT/lib/" "$GENERATED/libraylib/" "$GENERATED/libraylib_wrapper/"
 
 # Step 1: Build/obtain native library, need shared, linux and windows versions
 # Static libaries are prefered since a second level of dlopen can be skipped
@@ -53,16 +53,20 @@ $WINCC --shared -fpic -o "$OUTPUT/lib/libraylib_wrapper_x86_64-pc-windows-gnu.dl
 python3 third_party/Implib.so/implib-gen.py "$OUTPUT/lib/libraylib_wrapper_x86_64-unknown-linux-gnu.so" -o "$GENERATED/libraylib_wrapper/" --dlopen-callback cosmo_dlopen_wrapper --dlsym-callback cosmo_dlsym
 
 # Step 7: Build cosmo exec
-RAYLIBCOSMOCC="$COSMOCC -mcosmo src/dlopen_wrapper.c $GENERATED/libraylib/libraylib.so.cosmowrapper.c $GENERATED/libraylib_wrapper/* -I$OUTPUT/include/"
+RAYLIBCOSMOCC="$COSMOCC -mcosmo src/dlopen_wrapper.c -I$OUTPUT/include/ $GENERATED/libraylib/libraylib.so.cosmowrapper.c $GENERATED/libraylib_wrapper/*"
 
 $RAYLIBCOSMOCC -o "$OUTPUT/bin/shapes_basic_shapes.com" "$RAYLIBSRC/../examples/shapes/shapes_basic_shapes.c"
 # TODO: Fix, currently fails to compile because of redefined symbols
-#$RAYLIBCOSMOCC -o $OUTPUT/bin/core_3d_camera_first_person.com src/core_3d_camera_first_person.c
+#$RAYLIBCOSMOCC -o "$OUTPUT/bin/core_3d_camera_first_person.com" src/core_3d_camera_first_person.c
 $RAYLIBCOSMOCC -o "$OUTPUT/bin/core_3d_camera_split_screen.com" "$RAYLIBSRC/../examples/core/core_3d_camera_split_screen.c"
-# TODO: Readd raygui dependency, I have tested this and it does work
-#$RAYLIBCOSMOCC -o $OUTPUT/bin/controls_test_suite.com src/controls_test_suite.c
-# TODO: Add raygames dependency, again this does work
-#$RAYLIBCOSMOCC -o $OUTPUT/bin/snake.com src/snake.c
-# This one has broken controls and needs a few modifications to build with this system
-#$RAYLIBCOSMOCC -o $OUTPUT/bin/retro_maze.com src/retro_maze_3d/*.c
-#$RAYLIBCOSMOCC -o $OUTPUT/bin/first_person_maze.com src/models_first_person_maze/models_first_person_maze.c
+$RAYLIBCOSMOCC -o "$OUTPUT/bin/controls"_test_suite.com src/raygui_fix.c third_party/raygui/examples/controls_test_suite/controls_test_suite.c -Ithird_party/raygui/src/
+# TODO: Fix, currently complains about raylib's colours being missing, none of the other files do
+#sed 's/pause/paused/g;16 i #include <stdbool.h>' third_party/raylib-games/classics/src/tetris.c > "$GENERATED/tetris.c" && $RAYLIBCOSMOCC -o "$OUTPUT/bin/tetris.com" "$GENERATED/tetris.c"
+$RAYLIBCOSMOCC -o "$OUTPUT/bin/snake.com" third_party/raylib-games/classics/src/snake.c
+# TODO: Fix, currently has broken controls and needs a few modifications to build with this system
+#$RAYLIBCOSMOCC -o "$OUTPUT/bin/retro_maze.com" src/retro_maze_3d/*.c
+$RAYLIBCOSMOCC -o "$OUTPUT/bin/first_person_maze.com" "$RAYLIBSRC/../examples/models/models_first_person_maze.c" && cp --update "$RAYLIBSRC/../examples/models/resources/cubicmap.png" "$RAYLIBSRC/../examples/models/resources/cubicmap_atlas.png" "$OUTPUT/bin/resources/"
+
+# Temp fix to get windows working more easily
+# For linux "export LD_LIBRARY_PATH=$PWD/output/lib/:$PWD/third_party/raylib/src/:$LD_LIBRARY_PATH" is needed for the moment
+cp --update "$OUTPUT/lib/libraylib_wrapper_x86_64-pc-windows-gnu.dll" "$OUTPUT/bin/"
